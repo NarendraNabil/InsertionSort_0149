d177d27437c79eb38b3bf765a90f8d05665b5c25
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-03-25 16:20:14 +0700

    Menambahkan fungsi display untuk menampilkan array terurut

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -57,6 +57,24 @@ void insertionSort()
     }
 }
 
+void display()
+{
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Total Passs = " << n - 1 << endl;
+    cout << "======================" << endl;
+    cout << "Element array yang telah tersusun" << endl;
+    cout << "======================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+
+    cout << endl;
+
+}
+
 
 int main() {
     return 0;
