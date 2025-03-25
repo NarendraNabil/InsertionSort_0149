efa49c88a4156f248b709d125ba659a8ac4e3403
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-03-25 15:45:46 +0700

    Menambahkan fungsi input untuk menerima data array

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -4,6 +4,24 @@ using namespace std;
 int arr[20];
 int n;
 
+void input()
+{
+    cout  << "Masukkan Jumlah Data pada Array : ";
+    cin >> n;
+
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan element array" << endl;
+    cout << "======================" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+
 int main() {
     return 0;
 }
