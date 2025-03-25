35cd3984da0ed19091b430f94860c9501e95495a
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-03-25 16:11:32 +0700

    Implementasi fungsi InsertionSort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -37,6 +37,26 @@ void input()
    
 }
 
+void insertionSort() 
+{
+    int temp;
+    int j, i;
+
+    for (i = 1; i < n; i++)
+    {
+        temp = arr[i];
+        j = i -1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+    }
+}
+
 
 int main() {
     return 0;
